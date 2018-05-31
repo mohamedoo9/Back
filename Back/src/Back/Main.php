ae302cad2f86e4c7dfb1ff2436256f941b8ef9e2
mohamedoo9 <39798188+mohamedoo9@users.noreply.github.com>
2018-05-31 14:50:50 +0300

    Update Main.php

--- Back/src/Back/Main.php
+++ Back/src/Back/Main.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace Back;
+
+ use pocketmine\plugin\PluginBase;
+
+  class Main extends PluginBase{
+      
+      public function onEnable(){
+         $this->getServer()->getLogger()->info("back plugin has been enabled");
+     }
+   }
